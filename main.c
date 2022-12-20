8c37bfc303f08312b7eedcba899bf22442b73705
tanushka-jha23 <119748453+tanushka-jha23@users.noreply.github.com>
2022-12-20 10:24:45 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("%c", a);
+    scanf("%c", &a);
+
+    if(a=='A'||a== 'E'|| a=='I'||a== 'O'||a=='U'||a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+    {
+        printf("it's a vowel");
+    }
+    
+    else {   
+        printf("it's a consonant");
+    }
+
+    return 0;
+
+
+}
+  
+
+ 
+
+
+
+
+
+    
