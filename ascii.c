27192126cd019dfdaae116db1cdc52354007932f
tanushka-jha23 <119748453+tanushka-jha23@users.noreply.github.com>
2022-12-20 11:00:40 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char a; // taking input
+    scanf("%c", &a);
+
+    printf("ASCII value of %c is %d", a, a);// printing ASCII value of character
+
+    return 0;
+}
